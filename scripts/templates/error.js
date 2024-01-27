4d606b19ab00b288d22843016d8170918fdf7b75
aeonshad <aeonletter@gmail.com>
2024-01-27 10:49:38 +0100

    InnerText

--- scripts/templates/error.js
+++ scripts/templates/error.js
@@ -5,13 +5,14 @@
 function errorTemplate() {
     const $wrapper = document.createElement('div');
     $wrapper.setAttribute('class', 'error');
+
+    $wrapper.innerHTML = `<img class="error-img" src="images/notfound.png" alt="">
+    <h2 class="error-text"></h2>`;
+
     const textInput = document.querySelector('.form-search');
+    $wrapper.querySelector('.error-text').innerText = `Aucune recette ne contient '${textInput.value}' vous pouvez chercher «
+    tarte aux pommes », « poisson », etc.`;
+
     console.log(textInput);
-    const error = `
-    <img class="error-img" src="images/notfound.png" alt="">
-    <h2 class="error-text">Aucune recette ne contient '${textInput.value}' vous pouvez chercher «
-        tarte aux pommes », « poisson », etc.</h2>
-        `;
-    $wrapper.innerHTML = error;
     return $wrapper;
 }
